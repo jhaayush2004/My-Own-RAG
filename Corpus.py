ef6c1e4aed1740b475ac1041c68983de16c0164f
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2024-07-08 21:31:45 +0530

    Create Creating Corpus.py

---
+++ Corpus.py
@@ -0,0 +1,12 @@
+corpus_of_documents = [
+    "Take a leisurely walk in the park and enjoy the fresh air.",
+    "Visit a local museum and discover something new.",
+    "Attend a live music concert and feel the rhythm.",
+    "Go for a hike and admire the natural scenery.",
+    "Have a picnic with friends and share some laughs.",
+    "Explore a new cuisine by dining at an ethnic restaurant.",
+    "Take a yoga class and stretch your body and mind.",
+    "Join a local sports league and enjoy some friendly competition.",
+    "Attend a workshop or lecture on a topic you're interested in.",
+    "Visit an amusement park and ride the roller coasters."
+]
